0135ad5a5618623f772eb9e521752534629c3fec
Anouar Zerrik <an_zerrik@etu.enset-media.ac.ma>
2026-08-03 19:55:43 +0100

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        temp = []
+        if len(strs) == 0 :
+            return []
+        for x in strs:
+            temp.append(str(len(x)).zfill(4))
+            temp.append(x)
+        return res.join(temp)
+
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+
+        if len(s) == 0 :
+            return res.append("")
+
+        i = 0
+        while (i < len(s)) :
+            # prefix_start = i + 
+            prefix = int(s[i:i+4])
+            indx = i + 4
+            res.append(s[indx : indx+prefix])
+            i += prefix + 4
+        
+        return res
